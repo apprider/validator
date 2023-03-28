a9a16d76b185f4c799258b2f8ad38cfb47c4c081
Raj Kushawaha <raj.design@gmail.com>
2023-03-29 05:00:09 +0530

    Update validator_setup.sh

--- validator_setup.sh
+++ validator_setup.sh
@@ -10,16 +10,15 @@ echo
 echo
 
 # Install dependencies
-sudo apt install wget
-wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-source ~/.profile
+
+sudo apt-get update
+sudo apt-get install golang
+
 
 # Clone and build polygons-sdk
 git clone https://github.com/apprider/carechain && cd carechain
 echo "Building Go executable, please wait..."
-go build main.go
+go build main.go -o carechainnode
 
 # Initialize validator dir
 echo "Initializing validator directory.."
